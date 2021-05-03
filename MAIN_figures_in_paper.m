4970b0300c4649bdc4766ca8f679c94a30aa4e08
Roee Gilron <roee@RoeeG.local>
2021-05-03 16:13:55 -0700

    push new changs

---
+++ MAIN_figures_in_paper.m
@@ -0,0 +1,58 @@
+function MAIN_figures_in_paper()
+%% code to create the functions in the paper 
+% note that the paper was copyedited, and figure numbers were changed
+% compared to the revised version. Thus function figure names do not
+% currently match paper figure names. 
+% TO DO - fix function names 
+
+%% Figure 1 
+% Configuration of implanted hardware and method of data streaming 
+% Illustration 
+
+%% Figure 2 
+% Anatomical and physiological localization of subthalamic and cortical leads
+% Illustration / structural 
+
+%% Figure 3 
+% Decoding motor fluctuations from long-duration recordings at home
+rcsAtHome_figures_figure4()
+
+%% Figure 4 
+% Personalized oscillatory fingerprints: statistical significance in defined frequency bands for all individuals
+rcsAtHome_figures_figure5_0()
+
+%% Figure 5
+% Contribution of specific features and recording sites to the decoding of movement state for all five individuals
+rcsAtHome_figures_figure5_2()
+
+%% Figure 6
+% Adaptive DBS recorded at home using subcortical beta or cortical gamma control signals from two individual
+rcsAtHome_figures_figure7_v2()
+
+%% Extended Data Figure 1 
+%  Localization of leads in subthalamic nucleus and over precentral gyrus
+% Illustration / structural 
+
+%% Extended Data Figure 2
+% Over 2,600 hours of motor cortex and basal ganglia field potentials streamed in home environment 
+rcsAtHome_figures_figure3_0()
+
+%% Extended Data Figure 3 
+%  Brief in-clinic recordings demonstrate effects of leovodopa and movement
+rcsAtHome_figures_figure3()
+
+%% Extended Data Figure 4 
+% Power spectra used for Parkinsonian motor state decoding
+rcsAtHome_figures_figures1()
+
+%% Extended Data Figure 5 
+% Unsupervised clustering segregates neural data into specific behavioral states
+rcsAtHome_figures_figure6()
+
+%% Extended Data Figure 6
+% Sleep strongly affects neural biomarkers 
+ 
+%% Extended Data Figure 7
+
+
+end
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This is analyis code used to produce the figures in the [paper](https://www.nature.com/articles/s41587-021-00897-5).
 
-`MAIN.m` has a guide to reproducing the figures in the paper. 
+`MAIN_figures_in_paper` has a guide to code used in creating the figures in the paper. 
